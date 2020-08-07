c4394d734bad5dbf80402e2cc4be199a3eb5cf73
shazy89 <erdo.shazy123@gmail.com>
2020-08-07 12:42:55 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'store_admin/home'
+
+  get 'about', to: 'static#about'
+
   get 'home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
   get 'admin/invoice', to: 'store_admin#invoice'
+
+
 end
